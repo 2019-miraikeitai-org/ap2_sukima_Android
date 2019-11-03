80166222ff3faeb84c2083b3ccde0d89640491be
yunosuke <watanabeyunosuke@yus.local>
2019-11-03 23:47:55 +0900

    スポットとの距離が5m以下になったらcheck in画面へ遷移

---
+++ app/src/main/java/com/example/sukima_android/CheckIn.kt
@@ -0,0 +1,16 @@
+package com.example.sukima_android
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class CheckIn : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkin)
+
+    }
+}
