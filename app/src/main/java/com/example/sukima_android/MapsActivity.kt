aa8f4bc45ce60d659ac187561afc3c58f96214d0
Hiroki021 <b1017021@fun.ac.jp>
2019-11-08 17:51:47 +0900

    サーバからの値でスポットを表示

--- app/src/main/java/com/example/sukima_android/MapsActivity.kt
+++ app/src/main/java/com/example/sukima_android/MapsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.sukima_android
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
@@ -17,11 +19,20 @@ import com.google.android.gms.maps.model.*
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
 import com.google.android.gms.maps.model.LatLng
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.util.Log
 import android.widget.TextView
 
 import com.google.maps.android.SphericalUtil.*
 import kotlinx.android.synthetic.main.activity_maps.*
 import kotlinx.android.synthetic.main.activity_time_layout.*
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMarkerClickListener {
@@ -32,6 +43,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMarkerClickListe
     private lateinit var map: GoogleMap
     private lateinit var lastLocation: Location
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    //LatLang型の配列
+    private val point_new = arrayOfNulls<LatLng>(3)
 
 
     //拡大縮小機能の値
@@ -133,16 +147,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMarkerClickListe
                 currentLatLng(currentLatLng)
             }
 
+            //HitAPITaskを呼び出して、APIをたたく
+            HitAPITask().execute("http://160.16.103.99/spots")
+            //緯度経度を取得するまで待つ
+            Thread.sleep(10000)
+
+/*
             val point_new = arrayOfNulls<LatLng>(3)
             point_new[0] = LatLng(41.8417846, 140.7675603)
             point_new[1] = LatLng(41.8415718222, 140.767425299)
             point_new[2] = LatLng(41.8395136176, 140.76271534)
-
+*/
             val distance:MutableList<Int> = mutableListOf()
 
             point_new.forEachIndexed { i, point ->
                 val LatLngA = LatLng(location.latitude, location.longitude)
 
+                Log.d("test",point_new[0].toString())
 
                 distance.add(computeDistanceBetween(LatLngA, point).toInt())
 
@@ -189,6 +210,94 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMarkerClickListe
             startActivity(intent)
         }
     }
+
+
+    @SuppressLint("StaticFieldLeak")
+    inner class HitAPITask: AsyncTask<String, String, String>(){
+        override fun doInBackground(vararg params: String?): String? {
+            //ここでAPIを叩きます。バックグラウンドで処理する内容です。
+            var connection: HttpURLConnection? = null
+            var reader: BufferedReader? = null
+            val buffer: StringBuffer
+
+            try {
+                //param[0]にはAPIのURI(String)を入れます(後ほど)。
+                //AsynkTask<...>の一つめがStringな理由はURIをStringで指定するからです。
+                val url = URL(params[0])
+                connection = url.openConnection() as HttpURLConnection
+                connection.connect()  //ここで指定したAPIを叩いてみてます。
+
+                //ここから叩いたAPIから帰ってきたデータを使えるよう処理していきます。
+
+                //とりあえず取得した文字をbufferに。
+                val stream = connection.inputStream
+                reader = BufferedReader(InputStreamReader(stream))
+                buffer = StringBuffer()
+                var line: String?
+                while (true) {
+                    line = reader.readLine()
+                    if (line == null) {
+
+                        break
+                    }
+                    buffer.append(line)
+                    Log.d("CHECK", buffer.toString())
+                }
+
+                //ここからは、今回はJSONなので、いわゆるJsonをParseする作業（Jsonの中の一つ一つのデータを取るような感じ）をしていきます。
+
+                //先ほどbufferに入れた、取得した文字列
+                val jsonText = buffer.toString()
+
+                //JSONObjectを使って、まず全体のJSONObjectを取ります。
+                val parentJsonObj = JSONObject(jsonText)
+
+//以下追加かつテスト
+                val parentJsonArray = parentJsonObj.getJSONArray("spots")
+                val detailJsonObj = parentJsonArray.getJSONObject(0)
+
+
+//以下緯度経度のパース
+                val position = detailJsonObj.getJSONObject("position")
+                val longitude = position.getDouble("longitude")
+                val latitude = position.getDouble("latitude")
+
+                // 以下メインで使えるようにLatLng型のグローバルな配列にスポットの値を代入
+
+
+                point_new[0] = LatLng(latitude, longitude)
+                point_new[1] = LatLng(41.8415718222, 140.767425299)
+                point_new[2] = LatLng(41.8395136176, 140.76271534)
+
+
+                //以上緯度経度のパース
+
+                //ここから下は、接続エラーとかJSONのエラーとかで失敗した時にエラーを処理する為のものです。
+            } catch (e: MalformedURLException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+            //finallyで接続を切断してあげましょう。
+            finally {
+                connection?.disconnect()
+                try {
+                    reader?.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+            //失敗した時はnullやエラーコードなどを返しましょう。
+            return null
+        }
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (result == null) return
+        }
+    }
+
 }
 
 
