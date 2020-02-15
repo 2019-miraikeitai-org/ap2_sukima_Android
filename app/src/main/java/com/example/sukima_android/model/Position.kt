4fd44ff5248acdd81d1964375f89e92a6a558308
Hiroki021 <50481277+Hiroki021@users.noreply.github.com>
2020-02-16 04:05:31 +0900

    Revert "Iss#71"

--- app/src/main/java/com/example/sukima_android/model/Position.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sukima_android.model
-
-data class Position(
-    val latitude: Double,
-    val longitude: Double
-)
--- app/src/main/java/com/example/sukima_android/model/Spots.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sukima_android.model
-
-data class Spots(
-    val spots: List<Spot>,
-    val session_id:String
-)
